93c6a048a586a376d6e611031efb76ed3a775e60
DaProclaima <sn.itpro@gmail.com>
2020-02-15 03:08:09 +0100

    feat: back to a unique core method

--- index.js
+++ index.js
@@ -3,58 +3,18 @@ class BruteForce {
 
 	constructor() {
     this.charset = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
-    this.charsetIndex = 0
-    this.countSuiteLength = 0
-    this.suite= []
-    this.suiteIndex = 0
+    this.suite = []
 
     this.charset = this.charset.split("")
-    
 	}
-
-  /**
-  * Defines which charset is picked up
-  */
-  loopToCharset(i) {
-    this.charset[i]
-  }
-
-  /**
-  * Prepares the suite index onto point
-  */
-  pointSuiteIndex(i) {
-    return this.suiteIndex = i
-  } 
-
-  /**
-  * Once every charset was used in i index, we prepare the new suite length
-  */
-  addOneToSuiteLength() {
-    return this.countSuiteLength++
-  }
-
-  /**
-  * Once the new suite length is defined, we apply to Suite so that it has one more slot 
-  */
-  setSuiteLength(){
-    if(this.suite.length < 3) {
-     return this.suite[this.countSuiteLength] 
-    }
-  }
-
-  giveCharset(suite) {
-    for(let i = 0 ; i < this.charset.length; i++) {
-     suite = this.charset[i]
-     console.log(suite)
-    }
-  }
 
 
   testSuite() {
     for(let i = 0 ; i < 8 ; i++) {
-      this.pointSuiteIndex(i);
-      this.giveCharset(this.suite[this.suiteIndex])
-        // console.log(this.suite[this.suiteIndex])
+      for(let j = 0 ; j < this.charset.length; j++) {
+        this.suite[i] = this.charset[j]
+        console.log(this.suite.join(""))
+      }
     }   
 
 
