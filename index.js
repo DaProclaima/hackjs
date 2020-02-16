eefe5cf47ae471767e011f77a30de9707657ab18
DaProclaima <sn.itpro@gmail.com>
2020-02-16 01:51:05 +0100

    feat: gives all combination over 2 indexes. does not yet succeed to have all combo  for longer suite

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+let fs = require('fs');
+let EOL = require('os').EOL
+
 
 class BruteForce {
 
 	constructor() {
     this.charset = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
+    this.combinations = []
     this.suite = []
 
     this.charset = this.charset.split("")
@@ -13,16 +17,15 @@ class BruteForce {
   }
 
   testSuite() {
-
     // is used for removing a char to chars set in order to use the next char once one suite index used all combinations with previous char
     let leftCharset 
 
     let currentChar
 
     let previousChar
-
     //raises suite.length
-    for( let i = 0; i < 5; i++) {
+    for( let i = 0; i < 20; i++) {
+
       leftCharset = this.charset.slice()
       while(leftCharset.length > 0){
         // console.log('leftCharset.length = '+leftCharset.length)
@@ -32,27 +35,30 @@ class BruteForce {
         // console.log(this.suite)
         if(this.suite.length > 1) {
           previousChar = currentChar
-          this.suite[i-1] = previousChar
-          for(let j = 0 ; j < this.charset.length; j++) {
-            this.suite[i] = this.charset[j]
-            console.log(this.suite) 
-
+          for(let k = 0; k < this.suite.length; k++){
+            this.suite[k] = previousChar
+            for(let j = 0 ; j < this.charset.length; j++) {
+              this.suite[i] = this.charset[j]
+              this.combinations.push(EOL+this.suite)
+              console.log(this.suite.join('')) 
+            }
           }
         }
       }
-
     // when max length of suite is reached, stop increasing its length 
-      
     } 
   }
 
 
-  run(){
+   run(){
     this.testSuite()
+    fs.writeFile( 'log.txt',this.combinations, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+    // let file = new File(this.combinations, 'log.txt', {type: 'text/plain'})
   }
 }
 
 let bruteForce = new BruteForce()
 bruteForce.run()
-
-
