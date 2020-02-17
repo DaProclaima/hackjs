1c77cad46d72a8256e24e7709d3a22ce5e739c2a
DaProclaima <sn.itpro@gmail.com>
2020-02-17 13:13:34 +0100

    feat: gives all combination over 2 indexes. does not yet succeed to have all combo  for longer suite

--- index.js
+++ index.js
@@ -24,29 +24,49 @@ class BruteForce {
 
     let previousChar
     //raises suite.length
-    for( let i = 0; i < 20; i++) {
-
-      leftCharset = this.charset.slice()
-      while(leftCharset.length > 0){
-        // console.log('leftCharset.length = '+leftCharset.length)
-        // console.log('currentChar = '+currentChar)
-        currentChar = leftCharset.shift()
-        this.suite[i] = currentChar
-        // console.log(this.suite)
-        if(this.suite.length > 1) {
-          previousChar = currentChar
-          for(let k = 0; k < this.suite.length; k++){
+    // console.log(this.suite.length)
+    for( let i = 0; i < 3; i++) {
+      // console.log(this.suite.length)
+      if (this.suite.length < 1) {
+        for(let j = 0; j < this.charset.length; j++) {
+          this.suite[i] = this.charset[j]
+          this.combinations.push(EOL+this.suite)
+          // console.log(this.suite.join(''))
+          // console.log(i)
+        }
+      } else {
+        // console.log(i)
+        leftCharset = this.charset.slice()
+        while(leftCharset.length > 0) {
+          // console.log('i higherindex = ' + i + ' leftCharset.length = ' + leftCharset.length)
+          previousChar = leftCharset.shift()
+          // console.log(previousChar)
+          for(let k = 0; k <= i; k++) {
             this.suite[k] = previousChar
-            for(let j = 0 ; j < this.charset.length; j++) {
-              this.suite[i] = this.charset[j]
-              this.combinations.push(EOL+this.suite)
-              console.log(this.suite.join('')) 
+            // console.log(this.suite[k])
+            // console.log('k = '+ k)
+            // console.log('i higherindex = ' + i + ' leftCharset.length = ' + leftCharset.length + ' k should reach i = '+ k)
+
+            for(let l = 0; l <= i; l++) {
+            // console.log('i higherindex = ' + i + ' leftCharset.length = ' + leftCharset.length + ' k should reach i = '+ k + ' l should reach i  = '+ l)
+              if(l != k) {
+                // console.log('l = ' + l + ' while k = '+k)
+                // console.log('i higherindex = ' + i + ' leftCharset.length = ' + leftCharset.length + ' k should reach i = '+ k + ' l should reach i  = '+ l)
+
+                for( let m = 0; m < this.charset.length; m++) {
+                  // console.log('i higherindex = ' + i + ' leftCharset.length = ' + leftCharset.length + ' k should reach i = '+ k + ' l should reach i  = '+ l + ' m should reach 93 = ' + m)
+                  // console.log(this.charset[m])
+                  this.suite[l] = this.charset[m]
+                  // console.log(this.suite)
+                  this.combinations.push(EOL+this.suite)
+                  console.log(this.suite.join('')) 
+                }
+              }
             }
           }
-        }
-      }
-    // when max length of suite is reached, stop increasing its length 
-    } 
+        } 
+      } 
+    }
   }
 
 
@@ -62,3 +82,28 @@ class BruteForce {
 
 let bruteForce = new BruteForce()
 bruteForce.run()
+
+
+// for( let i = 0; i < 20; i++) {
+
+//       leftCharset = this.charset.slice()
+//       while(leftCharset.length > 0){
+//         // console.log('leftCharset.length = '+leftCharset.length)
+//         // console.log('currentChar = '+currentChar)
+//         currentChar = leftCharset.shift()
+//         this.suite[i] = currentChar
+//         // console.log(this.suite)
+//         if(this.suite.length > 1) {
+//           previousChar = currentChar
+//           for(let k = 0; k < this.suite.length; k++){
+//             this.suite[k] = previousChar
+//             for(let j = 0 ; j < this.charset.length; j++) {
+//               this.suite[i] = this.charset[j]
+//               this.combinations.push(EOL+this.suite)
+//               console.log(this.suite.join('')) 
+//             }
+//           }
+//         }
+//       }
+//     // when max length of suite is reached, stop increasing its length 
+//     } 
